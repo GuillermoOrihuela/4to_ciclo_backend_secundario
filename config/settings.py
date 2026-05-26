9865a4a2bcf82217696213534a4b5bac80012ac4
Juan Guillermo <nicheaventura1@gmail.com>
2026-05-25 22:10:38 -0500

    se agrego la configuracion con mysql2

--- config/settings.py
+++ config/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'may04get46226d_db',
-        'USER': 'may04get46226d_usuario',
-        'PASSWORD': '45092077',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
